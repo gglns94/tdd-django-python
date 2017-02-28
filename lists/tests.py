1f5117a74a64c200926440e4dc2621c454529afe
gglns94 <gglns94@naver.com>
2017-02-28 17:41:45 +0900

    should show changes to function_test.py home.html test.py nad views.py

--- lists/tests.py
+++ lists/tests.py
@@ -5,14 +5,22 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
+        # request = HttpRequest()
+        # response = home_page(request)
+
+        response = self.client.get('/')
+        '''html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.endswith('</html>'))'''
+
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
